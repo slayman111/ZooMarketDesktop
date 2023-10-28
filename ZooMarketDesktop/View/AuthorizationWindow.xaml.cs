92d0940a0481dee3ae3dc5175025a03b55c6daaa
Chepkasov Ruslan <chepkasov.ruslan01@gmail.com>
2023-10-28 20:17:52 +0300

    init

---
+++ ZooMarketDesktop/View/AuthorizationWindow.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ZooMarketDesktop.View
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class AuthorizationWindow : Window
+    {
+        public AuthorizationWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
